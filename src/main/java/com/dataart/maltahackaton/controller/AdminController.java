830cd2fe8081c20fd09839ecdf18964b40517f8c
Alexander Kuzmenko <alexander.kuzmenko@dataart.com>
2018-10-31 21:18:11 +0100

    Lottery functionality.

--- src/main/java/com/dataart/maltahackaton/controller/AdminController.java
+++ src/main/java/com/dataart/maltahackaton/controller/AdminController.java
@@ -1,9 +1,26 @@
 package com.dataart.maltahackaton.controller;
 
+import com.dataart.maltahackaton.domain.dto.LotteryDto;
+import com.dataart.maltahackaton.service.LotteryService;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @RequestMapping("admin")
 public class AdminController {
+
+    private final LotteryService lotteryService;
+
+    public AdminController(LotteryService lotteryService) {
+        this.lotteryService = lotteryService;
+    }
+
+    @PostMapping("createLottery")
+    @ResponseBody
+    public LotteryDto createLottery(@RequestBody LotteryDto lotteryDto) {
+        return lotteryService.createLottery(lotteryDto);
+    }
 }
--- src/main/java/com/dataart/maltahackaton/domain/dto/LotteryDto.java
+++ src/main/java/com/dataart/maltahackaton/domain/dto/LotteryDto.java
@@ -2,8 +2,13 @@ package com.dataart.maltahackaton.domain.dto;
 
 import com.dataart.maltahackaton.domain.LotteryStatus;
 import lombok.Data;
+import org.springframework.format.annotation.DateTimeFormat;
 
+import java.math.BigDecimal;
 import java.time.LocalDateTime;
+
+import static org.springframework.format.annotation.DateTimeFormat.ISO.DATE;
+import static org.springframework.format.annotation.DateTimeFormat.ISO.TIME;
 
 @Data
 public class LotteryDto {
@@ -14,7 +19,13 @@ public class LotteryDto {
     private String fundName;
     private String fundDescription;
     private LotteryStatus status;
+    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm")
     private LocalDateTime startDate;
+    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm")
     private LocalDateTime endDate;
+    private BigDecimal ticketPrice;
+    private Long ticketCount;
+    private BigDecimal donation;
+    private BigDecimal prizePool;
 
 }
