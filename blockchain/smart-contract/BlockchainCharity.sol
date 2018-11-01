05cf5bd5441b0abf4428e9dd5668dd0ee3f81ac8
chernikovmaksym <chernikovmaksym@gmail.com>
2018-11-01 10:35:04 +0100

    Contract alpha version

--- blockchain/smart-contract/BlockchainCharity.sol
+++ blockchain/smart-contract/BlockchainCharity.sol
@@ -1,6 +1,6 @@
-pragma solidity ^0.4.18;
+pragma solidity ^0.4.25;
 
-import "./oraclizeAPI.sol";
+import "github.com/oraclize/ethereum-api/oraclizeAPI.sol";
 
 contract owned {
     address public owner;
@@ -32,25 +32,25 @@ contract CharityLottery is owned, usingOraclize {
     uint public maintenanceFeeRate;
     uint public maxMaintenanceFee;
     uint public ticketPrice;
-    uint public currentTicketNumber = - 1;
+    int public currentTicketNumber = -1;
     uint public amountRaised = 0;
 
     uint public winnerRate;
     uint public charityRate;
 
     struct Ticket {
-        uint ticketNumber;
+        int ticketNumber;
         address holder;
     }
 
     mapping(address => Ticket[]) public holderTickets;
     Ticket[] public allTickets;
 
-    uint public winnerTicketNumber = - 1; // use -1 for undefined
+    int public winnerTicketNumber = - 1; // use -1 for undefined
 
-    event BuyTicket(address holder, uint ticketNumber, uint ticketPrice);
+    event BuyTicket(address holder, int ticketNumber, uint ticketPrice);
     event WinnerTransfer(address winner, uint amount);
-    event BeneficiaryTransfer(address beneficiary, uint amount);
+    event CharityTransfer(address beneficiary, uint amount);
 
     modifier finishedLottery() {
         require(lotteryClosed == true);
@@ -88,7 +88,7 @@ contract CharityLottery is owned, usingOraclize {
         charityRate = charityPercent;
     }
 
-    function() payable public {
+    function() payable {
         require(!lotteryClosed);
 
         uint amount = msg.value;
@@ -96,12 +96,12 @@ contract CharityLottery is owned, usingOraclize {
 
         require(amount % ticketPrice == 0);
 
-        amountToReturn = amount % ticketPrice;
-        ticketAmount = amount / ticketPrice;
+        uint amountToReturn = amount % ticketPrice;
+        uint ticketAmount = amount / ticketPrice;
 
         for (uint i = 0; i < ticketAmount; i++) {
             currentTicketNumber++;
-            Ticket ticket = Ticket(currentTicketNumber, holder);
+            Ticket memory ticket = Ticket(currentTicketNumber, holder);
 
             amountRaised += ticketPrice;
             allTickets.push(ticket);
@@ -116,7 +116,7 @@ contract CharityLottery is owned, usingOraclize {
         bytes32 queryId = oraclize_newRandomDSQuery(oraclizeDelay, oraclizeRandomBytesAmount, callbackGas);
 
         // Needed to check in callback function
-        validIds[queryId] = true;
+        validOraclizeIds[queryId] = true;
     }
 
     /*
@@ -126,18 +126,18 @@ contract CharityLottery is owned, usingOraclize {
     function __callback(bytes32 _queryId, string _result, bytes _proof)
     {
         if (msg.sender != oraclize_cbAddress()
-        || !validIds[_queryId]
+        || !validOraclizeIds[_queryId]
         || oraclize_randomDS_proofVerify__returnCode(_queryId, _result, _proof) == 0) {
             throw;
         } else {
-            uint maxRange = currentTicketNumber;
-            winnerTicketNumber = uint(sha3(_result)) % maxRange;
+            int maxRange = currentTicketNumber;
+            winnerTicketNumber = int(sha3(_result)) % maxRange;
 
             finishLottery();
         }
     }
 
-    function finishLottery() payable internal isReadyToBeFinished {
+    function finishLottery() internal nonFinishedLottery isReachedDeadline {
         uint balance = address(this).balance;
         uint feeAmount = balance * maintenanceFeeRate / 100;
 
@@ -155,14 +155,15 @@ contract CharityLottery is owned, usingOraclize {
 
     function calculateAndSendWinnerAmount(uint giveAwayAmount) internal finishedLottery returns (uint charityDonationAmount) {
         uint winnerAmount = giveAwayAmount * winnerRate / 100;
-        allTickets[winnerTicketNumber].send(winnerAmount);
-        // TODO: emit events
+        address winnerAddress = allTickets[uint(winnerTicketNumber)].holder;
+        winnerAddress.send(winnerAmount);
+        emit WinnerTransfer(winnerAddress, winnerAmount);
         return giveAwayAmount - winnerAmount;
     }
 
     function calculateAndSendCharityAmount(uint charityDonationAmount) internal finishedLottery {
         charityFund.send(charityDonationAmount);
-        // TODO: emit events
+        emit CharityTransfer(charityFund, charityDonationAmount);
     }
 
     function withdrawOwnersAmount() onlyOwner finishedLottery {
