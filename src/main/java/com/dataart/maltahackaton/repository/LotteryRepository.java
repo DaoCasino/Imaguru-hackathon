2e87f16bf2d88102da8c0203ae65bc4684be9515
Vladislav Dmitriev <vladislav.a.dmitriev@gmail.com>
2018-11-01 20:55:29 +0100

    Lottery finish job.

--- src/main/java/com/dataart/maltahackaton/repository/LotteryRepository.java
+++ src/main/java/com/dataart/maltahackaton/repository/LotteryRepository.java
@@ -4,9 +4,10 @@ import com.dataart.maltahackaton.domain.Lottery;
 import com.dataart.maltahackaton.domain.LotteryStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 public interface LotteryRepository extends JpaRepository<Lottery, Long> {
 
-    List<Lottery> findAllByStatus(LotteryStatus status);
+    List<Lottery> findAllByStatusAAndEndDateBefore(LotteryStatus status, LocalDateTime localDateTime);
 }
---
+++ src/main/java/com/dataart/maltahackaton/service/LotteryFinishJob.java
@@ -0,0 +1,62 @@
+package com.dataart.maltahackaton.service;
+
+import com.dataart.maltahackaton.blockchain.BlockchainCharity;
+import com.dataart.maltahackaton.blockchain.BlockchainConfig;
+import com.dataart.maltahackaton.blockchain.LotteryProvider;
+import com.dataart.maltahackaton.domain.Lottery;
+import com.dataart.maltahackaton.domain.LotteryStatus;
+import com.dataart.maltahackaton.repository.LotteryRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Slf4j
+@Service
+public class LotteryFinishJob {
+
+    private final LotteryRepository lotteryRepository;
+    private final LotteryProvider lotteryProvider;
+    private final BlockchainConfig blockchainConfig;
+
+    public LotteryFinishJob(LotteryRepository lotteryRepository, LotteryProvider lotteryProvider, BlockchainConfig blockchainConfig) {
+        this.lotteryRepository = lotteryRepository;
+        this.lotteryProvider = lotteryProvider;
+        this.blockchainConfig = blockchainConfig;
+    }
+
+    @Scheduled(cron = "${lottery-finish.job.cron}")
+    public void executeJob() {
+        log.info("Starting lotteries sync...");
+        List<Lottery> potentiallyFinishedLotteries =
+                lotteryRepository.findAllByStatusAAndEndDateBefore(LotteryStatus.ACTIVE, LocalDateTime.now());
+        log.info("Found {} active lotteries", potentiallyFinishedLotteries.size());
+        log.info("Starting finish lotteries...");
+
+        for (Lottery lottery : potentiallyFinishedLotteries) {
+            BlockchainCharity blockchainCharity = lotteryProvider.loadFromOwner(lottery.getContractAddress(),
+                    blockchainConfig.getGasPrice(), blockchainConfig.getGasLimit());
+            try {
+                if (blockchainCharity.lotteryClosed().send()) {
+                    log.info("Finishing lottery {}", lottery.getContractAddress());
+                    blockchainCharity.finishLottery().send();
+                    lottery.setStatus(LotteryStatus.INACTIVE);
+                    lotteryRepository.save(lottery);
+                    log.info("Lottery finished...");
+                } else {
+                    log.info("Withdrawing funds from closed lottery to wallet: {}", blockchainConfig.getOwnerWalletAddress());
+                    blockchainCharity.withdrawOwnersAmount().send();
+                    lottery.setCompleted(true);
+                    lotteryRepository.save(lottery);
+                    log.info("Withdrawing completed...");
+                }
+            } catch (Exception e) {
+                log.error("Error while finishing lottery", e);
+            }
+        }
+        log.info("Finishing of lotteries is completed...");
+        log.info("Lotteries sync finished...");
+    }
+}
