fc50d89ee4939b641cadc8917d96483e628f2ca2
Alexander Kuzmenko <alexander.kuzmenko@dataart.com>
2018-11-02 00:13:50 +0100

    Login/Logout

--- src/main/java/com/dataart/maltahackaton/controller/BaseController.java
+++ src/main/java/com/dataart/maltahackaton/controller/BaseController.java
@@ -6,8 +6,18 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class BaseController {
 
+    @GetMapping("/")
+    public String homeRedirect() {
+        return "redirect:/lotteries/getAll";
+    }
+
     @GetMapping("games")
     public String getGamesPage() {
         return "games";
+    }
+
+    @GetMapping("login")
+    public String login() {
+        return "login";
     }
 }
