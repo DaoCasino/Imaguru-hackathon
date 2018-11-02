b956fe63f4dc9b3fba3885afe8825eed53467e73
chernikovmaksym <chernikovmaksym@gmail.com>
2018-11-02 12:30:25 +0100

    Removing unnecessary testing endpoints

--- src/main/java/com/dataart/maltahackaton/controller/LotteryController.java
+++ src/main/java/com/dataart/maltahackaton/controller/LotteryController.java
@@ -1,8 +1,5 @@
 package com.dataart.maltahackaton.controller;
 
-import com.dataart.maltahackaton.blockchain.BlockchainCharity;
-import com.dataart.maltahackaton.blockchain.BlockchainConfig;
-import com.dataart.maltahackaton.blockchain.LotteryProvider;
 import com.dataart.maltahackaton.domain.dto.LotteryResponse;
 import com.dataart.maltahackaton.service.LotteryService;
 import lombok.extern.slf4j.Slf4j;
@@ -13,10 +10,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.web3j.protocol.Web3j;
-import org.web3j.protocol.core.methods.response.TransactionReceipt;
 
-import java.math.BigInteger;
 import java.util.List;
 
 @Slf4j
@@ -24,44 +18,11 @@ import java.util.List;
 @RequestMapping("lotteries")
 public class LotteryController {
 
-    private final LotteryProvider lotteryProvider;
     private final LotteryService lotteryService;
-    private final BlockchainConfig blockchainConfig;
-    private final Web3j web3j;
 
-    public LotteryController(LotteryProvider lotteryProvider, LotteryService lotteryService, BlockchainConfig blockchainConfig, Web3j web3j) {
-        this.lotteryProvider = lotteryProvider;
+    public LotteryController(LotteryService lotteryService) {
         this.lotteryService = lotteryService;
-        this.blockchainConfig = blockchainConfig;
-        this.web3j = web3j;
     }
-
-    // TODO: Remove from this
-    @ResponseBody
-    @GetMapping("deploy")
-    public String deploy() throws Exception {
-        return lotteryProvider.deploy("0xfed7907edc850959e570092ac0c45726f48978dd", new BigInteger("3"),
-                BigInteger.TEN, new BigInteger("100000000000000000"), new BigInteger("1000000000000000"), BigInteger.TEN);
-    }
-
-    @ResponseBody
-    @GetMapping("finish/{contractAddress}")
-    public void finish(@PathVariable String contractAddress) throws Exception {
-        BlockchainCharity contract = lotteryProvider.loadFromOwner(contractAddress);
-        TransactionReceipt transactionReceipt = contract.finishLottery().send();
-        log.info(transactionReceipt.getBlockNumber().toString());
-        log.info(transactionReceipt.getTransactionHash());
-        log.info("lottery finished");
-    }
-
-    @ResponseBody
-    @GetMapping("withdrawal/{contractAddress}")
-    public void withdrawal(@PathVariable String contractAddress) throws Exception {
-        BlockchainCharity contract = lotteryProvider.loadFromOwner(contractAddress);
-        contract.withdrawOwnersAmount().send();
-        log.info("Owner is rich now");
-    }
-    // TODO: Remove to this
 
     @GetMapping("getAll")
     public String getAll(Model model) {
